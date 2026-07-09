ad41bdf5210c2156f9514cb58cbb0f637bd3b82a
Akshat <akshatkutariyar@gmail.com>
2026-07-09 22:44:19 +0530

    Implemented global logic

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable Global Validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.useGlobalFilters(new AllExceptionsFilter()); // 2. Apply it
 
   // Swagger Configuration
   const config = new DocumentBuilder()
